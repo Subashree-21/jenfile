896b528c3c643387623eea50d4211861f55d40f1
Subashree-21 <23203032@srcas.ac.in>
2024-07-01 11:58:11 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+class sample{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello");
+    System.out.println("welcome");
+    System.out.println("bye");
+    System.out.println("hii");
+  }
+}
+    
